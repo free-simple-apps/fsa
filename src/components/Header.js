1c74f3163fb1d66e33d2df377ce519c319a2af44
Serge Belychev <44494423+belychev@users.noreply.github.com>
2019-05-05 13:32:15 +0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ const Header = props => (
     <h1>Free Simple Apps</h1>
     <p style="margin-bottom:2px;">We make apps for easy, cheerful and healthy life.</p>
     <p>
-      <a href="https://play.google.com/store/apps/developer?id=Free+Simple+Apps" style="font-weight:bold">Find our apps in Google Play!</a>
+      <a href="https://play.google.com/store/apps/developer?id=Free+Simple+Apps" style="font-weight:bold">Find our apps in Google Play.</a>
     </p>
   </header>
 )
